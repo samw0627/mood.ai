172fe681b9aae76a6d3323ecf25d308eab270549
Sam Wong <97695769+samw0627@users.noreply.github.com>
2024-01-31 23:46:01 -0800

    Incorporate DeepChat

--- app/App.jsx
+++ app/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
+import { DeepChat } from "deep-chat-react";
 
 function App() {
-    return (
-        <div>
-            This is a new React App
-        </div>
-    );
+  const initialMessages = [
+    { role: "user", text: "Hey, how are you today?" },
+    { role: "ai", text: "I am doing very well!" },
+  ];
+  return (
+    <DeepChat
+      demo={true}
+      style={{ borderRadius: "10px" }}
+      textInput={{ placeholder: { text: "Welcome to the demo!" } }}
+      initialMessages={initialMessages}
+    />
+  );
 }
 
 export default App;
--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { StrictMode } from "react";
 import App from "./App";
 
 
@@ -7,8 +6,6 @@ import App from "./App";
 
 export default function Home() {
   return (
-    <StrictMode>
       <App />
-    </StrictMode>
   );
 }
